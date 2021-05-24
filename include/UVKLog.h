a959a3ae07937ca494b8dd6804e6f79c9db06564
Stanislav Vasilev <4ipsdaqde6@gmail.com>
2021-05-24 19:04:28 +0300

    Update UVKLog.h

--- include/UVKLog.h
+++ include/UVKLog.h
@@ -26,6 +26,40 @@ enum LogType
     UVK_LOG_TYPE_MESSAGE
 };
 
+class Timer
+{
+public:
+    void startRecording()
+    {
+        start = std::chrono::high_resolution_clock::now();
+    }
+
+    void stopRecording()
+    {
+        auto endTime = std::chrono::high_resolution_clock::now();
+        auto st = std::chrono::time_point_cast<std::chrono::microseconds>(start).time_since_epoch().count();
+        auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTime).time_since_epoch().count();
+
+        auto dt = end - st;
+
+        duration = (double)dt * 0.001;
+    }
+
+    ~Timer()
+    {
+        stopRecording();
+    }
+
+    [[nodiscard]] double getDuration() const
+    {
+        return duration;
+    }
+private:
+    double duration = 0;
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+};
+
 class UVKLog
 {
 public:
